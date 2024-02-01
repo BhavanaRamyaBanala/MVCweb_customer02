9589caf0f6615d43686799f587d55e11c1051a63
BhavanaRamyaBanala <ramyabanala12@gmail.com>
2024-02-01 17:39:09 +0530

    First commit

---
+++ WebApp_customerLayout/Controllers/CustomerController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp_customerLayout.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Custom
+        public ActionResult Index()
+        {
+            ViewBag.msg = "Customer Home Page";
+            return View();
+        }
+        public ActionResult NameList() {
+            List<string> list = new List<string>() {
+                "Sravani",
+                "Mani",
+                "Jagadeesh",
+                "Ramki"
+            };
+            ViewBag.nameList = list;
+            return View();
+        }
+    }
+}
